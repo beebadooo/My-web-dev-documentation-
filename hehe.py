fbe57b08aabcc2839bfe0bee3981ad1203834beb
Mishka <mishka160506@gmail.com>
2025-01-13 00:40:24 +0530

    py

--- hehe.py
+++ hehe.py
@@ -1,9 +1,24 @@
-import turtle as t
-window = t.Screen()
+# import turtle as t
+# window = t.Screen()
 
-hey = t.Turtle()
-t.bgcolor('purple')
+# hey = t.Turtle()
+# t.bgcolor('purple')
 
 
-window.exitonclick()
-turtle.done()
+# window.exitonclick()
+# turtle.done()
+
+
+# WE PREP
+# find missing no in an array of range [0,n]
+
+def missingno(arr):
+    n = len(arr)
+    total = (n*(n+1))//2
+    sum = 0
+    for i in arr:
+        sum+=i
+    return total-sum
+
+arr=[3,0,1]
+print(missingno(arr))
