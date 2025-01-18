8ff357c943daad28d03845f5dac970b648fec4a6
Mishka <mishka160506@gmail.com>
2025-01-18 18:20:06 +0530

    python

--- hehe.py
+++ hehe.py
@@ -12,13 +12,31 @@
 # WE PREP
 # find missing no in an array of range [0,n]
 
-def missingno(arr):
-    n = len(arr)
-    total = (n*(n+1))//2
-    sum = 0
-    for i in arr:
-        sum+=i
-    return total-sum
+# def missingno(arr):
+#     n = len(arr)
+#     total = (n*(n+1))//2
+#     sum = 0
+#     for i in arr:
+#         sum+=i
+#     return total-sum
 
-arr=[3,0,1]
-print(missingno(arr))
+# arr=[3,0,1]
+# print(missingno(arr))
+
+# remove parenthesis
+def removeOuterParentheses(s: str) -> str:
+        result = []
+        c = 0
+        for i in s:
+            if i == '(':
+                if c>0:
+                    result.append(i)
+                c+=1
+            elif i == ')':
+                c -= 1
+                if c>0:
+                    result.append(i)
+        return "".join(result)
+
+s = "(()())(())"
+print(removeOuterParentheses(s))
